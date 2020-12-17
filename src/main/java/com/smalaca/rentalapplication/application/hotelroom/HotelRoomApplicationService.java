1baef76c4ae8de6cdcb549254d8dc6feaff4b606
smalaca <ab13.krakow@gmail.com>
2020-12-17 22:17:50 +0100

    Adding new HotelRoom - introduce HotelRoomRepository

--- src/main/java/com/smalaca/rentalapplication/application/hotelroom/HotelRoomApplicationService.java
+++ src/main/java/com/smalaca/rentalapplication/application/hotelroom/HotelRoomApplicationService.java
@@ -2,11 +2,20 @@ package com.smalaca.rentalapplication.application.hotelroom;
 
 import com.smalaca.rentalapplication.domain.hotelroom.HotelRoom;
 import com.smalaca.rentalapplication.domain.hotelroom.HotelRoomFactory;
+import com.smalaca.rentalapplication.domain.hotelroom.HotelRoomRepository;
 
 import java.util.Map;
 
 public class HotelRoomApplicationService {
+    private final HotelRoomRepository hotelRoomRepository;
+
+    public HotelRoomApplicationService(HotelRoomRepository hotelRoomRepository) {
+        this.hotelRoomRepository = hotelRoomRepository;
+    }
+
     public void add(String hotelId, int number, Map<String, Double> spacesDefinition, String description) {
         HotelRoom hotelRoom = new HotelRoomFactory().create(hotelId, number, spacesDefinition, description);
+
+        hotelRoomRepository.save(hotelRoom);
     }
 }
---
+++ src/main/java/com/smalaca/rentalapplication/domain/hotelroom/HotelRoomRepository.java
@@ -0,0 +1,5 @@
+package com.smalaca.rentalapplication.domain.hotelroom;
+
+public interface HotelRoomRepository {
+    void save(HotelRoom hotelRoom);
+}
