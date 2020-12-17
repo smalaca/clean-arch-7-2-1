c8d77bf6305eb96583ec975a4a85e6c49e63a00f
smalaca <ab13.krakow@gmail.com>
2020-12-17 20:43:52 +0100

    Adding new Apartment - move creation of Apartment from ApartmentApplicationService to ApartmentFactory

--- src/main/java/com/smalaca/rentalapplication/application/apartment/ApartmentApplicationService.java
+++ src/main/java/com/smalaca/rentalapplication/application/apartment/ApartmentApplicationService.java
@@ -1,26 +1,16 @@
 package com.smalaca.rentalapplication.application.apartment;
 
-import com.smalaca.rentalapplication.domain.apartment.Address;
 import com.smalaca.rentalapplication.domain.apartment.Apartment;
-import com.smalaca.rentalapplication.domain.apartment.Room;
-import com.smalaca.rentalapplication.domain.apartment.SquareMeter;
+import com.smalaca.rentalapplication.domain.apartment.ApartmentFactory;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class ApartmentApplicationService {
     public void add(
             String ownerId, String street, String postalCode, String houseNumber, String apartmentNumber,
             String city, String country, String description, Map<String, Double> roomsDefinition) {
-        
-        Address address = new Address(street, postalCode, houseNumber, apartmentNumber, city, country);
-        List<Room> rooms = new ArrayList<>();
-        roomsDefinition.forEach((name, size) -> {
-            SquareMeter squareMeter = new SquareMeter(size);
-            rooms.add(new Room(name, squareMeter));
-        });
 
-        Apartment apartment = new Apartment(ownerId, address, description);
+        Apartment apartment = new ApartmentFactory().create(
+                ownerId, street, postalCode, houseNumber, apartmentNumber, city, country, description, roomsDefinition);
     }
 }
