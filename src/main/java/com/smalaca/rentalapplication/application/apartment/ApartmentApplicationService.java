ec9ec3b6dcc2f7f2560c5d228ccf7383d6e84fc5
smalaca <ab13.krakow@gmail.com>
2020-12-17 21:48:00 +0100

    Adding new Apartment - save newly created Apartment in ApartmentRepository

--- src/main/java/com/smalaca/rentalapplication/application/apartment/ApartmentApplicationService.java
+++ src/main/java/com/smalaca/rentalapplication/application/apartment/ApartmentApplicationService.java
@@ -2,15 +2,24 @@ package com.smalaca.rentalapplication.application.apartment;
 
 import com.smalaca.rentalapplication.domain.apartment.Apartment;
 import com.smalaca.rentalapplication.domain.apartment.ApartmentFactory;
+import com.smalaca.rentalapplication.domain.apartment.ApartmentRepository;
 
 import java.util.Map;
 
 public class ApartmentApplicationService {
+    private final ApartmentRepository apartmentRepository;
+
+    public ApartmentApplicationService(ApartmentRepository apartmentRepository) {
+        this.apartmentRepository = apartmentRepository;
+    }
+
     public void add(
             String ownerId, String street, String postalCode, String houseNumber, String apartmentNumber,
             String city, String country, String description, Map<String, Double> roomsDefinition) {
 
         Apartment apartment = new ApartmentFactory().create(
                 ownerId, street, postalCode, houseNumber, apartmentNumber, city, country, description, roomsDefinition);
+
+        apartmentRepository.save(apartment);
     }
 }
---
+++ src/main/java/com/smalaca/rentalapplication/domain/apartment/ApartmentRepository.java
@@ -0,0 +1,5 @@
+package com.smalaca.rentalapplication.domain.apartment;
+
+public interface ApartmentRepository {
+    void save(Apartment apartment);
+}
