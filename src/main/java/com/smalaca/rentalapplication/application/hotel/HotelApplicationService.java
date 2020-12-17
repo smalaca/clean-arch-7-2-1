f569e27b07c383e47845256010b7ddce776395b2
smalaca <ab13.krakow@gmail.com>
2020-12-17 21:06:08 +0100

    Adding new Hotel - move creation of Hotel from HotelApplicationService to HotelFactory

--- src/main/java/com/smalaca/rentalapplication/application/hotel/HotelApplicationService.java
+++ src/main/java/com/smalaca/rentalapplication/application/hotel/HotelApplicationService.java
@@ -1,11 +1,10 @@
 package com.smalaca.rentalapplication.application.hotel;
 
-import com.smalaca.rentalapplication.domain.hotel.Address;
 import com.smalaca.rentalapplication.domain.hotel.Hotel;
+import com.smalaca.rentalapplication.domain.hotel.HotelFactory;
 
 public class HotelApplicationService {
     public void add(String name, String street, String postalCode, String buildingNumber, String city, String country) {
-        Address address = new Address(street, postalCode, buildingNumber, city, country);
-        Hotel hotel = new Hotel(name, address);
+        Hotel hotel = new HotelFactory().create(name, street, postalCode, buildingNumber, city, country);
     }
 }
---
+++ src/main/java/com/smalaca/rentalapplication/domain/hotel/HotelFactory.java
@@ -0,0 +1,8 @@
+package com.smalaca.rentalapplication.domain.hotel;
+
+public class HotelFactory {
+    public Hotel create(String name, String street, String postalCode, String buildingNumber, String city, String country) {
+        Address address = new Address(street, postalCode, buildingNumber, city, country);
+        return new Hotel(name, address);
+    }
+}
