18b15843ecc3ab60423ba4c87662091a5391eebe
smalaca <ab13.krakow@gmail.com>
2020-12-19 02:36:32 +0100

    Booking reject - introduce implementation of CommandRegistry

---
+++ src/main/java/com/smalaca/rentalapplication/infrastructure/commandregistry/spring/SpringCommandRegistry.java
@@ -0,0 +1,24 @@
+package com.smalaca.rentalapplication.infrastructure.commandregistry.spring;
+
+import com.smalaca.rentalapplication.application.booking.BookingAccept;
+import com.smalaca.rentalapplication.application.booking.BookingReject;
+import com.smalaca.rentalapplication.application.commandregistry.CommandRegistry;
+import org.springframework.context.ApplicationEventPublisher;
+
+class SpringCommandRegistry implements CommandRegistry {
+    private final ApplicationEventPublisher publisher;
+
+    SpringCommandRegistry(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public void register(BookingReject bookingReject) {
+        publisher.publishEvent(bookingReject);
+    }
+
+    @Override
+    public void register(BookingAccept bookingAccept) {
+        publisher.publishEvent(bookingAccept);
+    }
+}
