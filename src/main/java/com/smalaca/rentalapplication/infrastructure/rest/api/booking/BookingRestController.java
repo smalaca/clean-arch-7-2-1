9ba9ff2c74d9b8b16048253a39a10e3e6a96af8a
smalaca <ab13.krakow@gmail.com>
2020-12-19 02:28:42 +0100

    FIX - add @PathVariable annotation to read parameter from url

--- src/main/java/com/smalaca/rentalapplication/infrastructure/rest/api/booking/BookingRestController.java
+++ src/main/java/com/smalaca/rentalapplication/infrastructure/rest/api/booking/BookingRestController.java
@@ -3,6 +3,7 @@ package com.smalaca.rentalapplication.infrastructure.rest.api.booking;
 import com.smalaca.rentalapplication.application.booking.BookingAccept;
 import com.smalaca.rentalapplication.application.booking.BookingReject;
 import com.smalaca.rentalapplication.application.commandregistry.CommandRegistry;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,12 @@ public class BookingRestController {
     }
 
     @PutMapping("/reject/{id}")
-    public void reject(String id) {
+    public void reject(@PathVariable String id) {
         commandRegistry.register(new BookingReject(id));
     }
 
     @PutMapping("/accept/{id}")
-    public void accept(String id) {
+    public void accept(@PathVariable String id) {
         commandRegistry.register(new BookingAccept(id));
     }
 }
