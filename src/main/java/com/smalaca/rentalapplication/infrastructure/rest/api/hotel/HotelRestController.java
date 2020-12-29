d68fde17d7d23db356952002c8a6f45fa5e32672
smalaca <ab13.krakow@gmail.com>
2020-12-29 21:43:22 +0100

    comment out @RestController annotation to hide problem with configuration of dependency injection

--- src/main/java/com/smalaca/rentalapplication/infrastructure/rest/api/hotel/HotelRestController.java
+++ src/main/java/com/smalaca/rentalapplication/infrastructure/rest/api/hotel/HotelRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 @RequestMapping("/hotel")
 public class HotelRestController {
     private final HotelApplicationService hotelApplicationService;
