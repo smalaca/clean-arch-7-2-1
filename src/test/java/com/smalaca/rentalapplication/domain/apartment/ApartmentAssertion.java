1dc099a36e55d6330fb1bf176236d5ae4248da9e
smalaca <ab13.krakow@gmail.com>
2020-12-30 20:51:00 +0100

    Apartment - book - introduce ApartmentAssertion

---
+++ src/test/java/com/smalaca/rentalapplication/domain/apartment/ApartmentAssertion.java
@@ -0,0 +1,62 @@
+package com.smalaca.rentalapplication.domain.apartment;
+
+import org.assertj.core.api.Assertions;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+class ApartmentAssertion {
+    private final Apartment actual;
+
+    private ApartmentAssertion(Apartment actual) {
+        this.actual = actual;
+    }
+
+    static ApartmentAssertion assertThat(Apartment actual) {
+        return new ApartmentAssertion(actual);
+    }
+
+
+    ApartmentAssertion hasOwnerIdEqualsTo(String ownerId) {
+        Assertions.assertThat(actual).hasFieldOrPropertyWithValue("ownerId", ownerId);
+        return this;
+    }
+
+    ApartmentAssertion hasDescriptionEqualsTo(String description) {
+        Assertions.assertThat(actual).hasFieldOrPropertyWithValue("description", description);
+        return this;
+    }
+
+    ApartmentAssertion hasAddressEqualsTo(
+            String street, String postalCode, String houseNumber, String apartmentNumber, String city, String country) {
+        Assertions.assertThat(actual).extracting("address")
+                .hasFieldOrPropertyWithValue("street", street)
+                .hasFieldOrPropertyWithValue("postalCode", postalCode)
+                .hasFieldOrPropertyWithValue("houseNumber", houseNumber)
+                .hasFieldOrPropertyWithValue("apartmentNumber", apartmentNumber)
+                .hasFieldOrPropertyWithValue("city", city)
+                .hasFieldOrPropertyWithValue("country", country);
+
+        return this;
+    }
+
+    ApartmentAssertion hasRoomsEqualsTo(Map<String, Double> roomsDefinition) {
+        Assertions.assertThat(actual).extracting("rooms").satisfies(roomsActual -> {
+            List<Room> rooms = (List<Room>) roomsActual;
+            Assertions.assertThat(rooms).hasSize(roomsDefinition.size());
+
+            roomsDefinition.forEach((name, squareMeter) -> {
+                Assertions.assertThat(rooms).anySatisfy(hasRoomThat(name, squareMeter));
+            });
+        });
+
+        return this;
+    }
+
+    private Consumer<Room> hasRoomThat(String name, Double squareMeter) {
+        return room -> Assertions.assertThat(room)
+                .hasFieldOrPropertyWithValue("name", name)
+                .hasFieldOrPropertyWithValue("squareMeter.size", squareMeter);
+    }
+}
--- src/test/java/com/smalaca/rentalapplication/domain/apartment/ApartmentTest.java
+++ src/test/java/com/smalaca/rentalapplication/domain/apartment/ApartmentTest.java
@@ -3,11 +3,7 @@ package com.smalaca.rentalapplication.domain.apartment;
 import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class ApartmentTest {
     private static final String OWNER_ID = "1234";
@@ -20,52 +16,18 @@ class ApartmentTest {
     private static final String DESCRIPTION = "Nice place to stay";
     private static final Map<String, Double> ROOMS_DEFINITION = ImmutableMap.of("Toilet", 10.0, "Bedroom", 30.0);
 
+    private final ApartmentFactory apartmentFactory = new ApartmentFactory();
+
     @Test
     void shouldCreateApartmentWithAllRequiredFields() {
-        Apartment actual = new ApartmentFactory().create(
+        Apartment actual = apartmentFactory.create(
                 OWNER_ID, STREET, POSTAL_CODE, HOUSE_NUMBER, APARTMENT_NUMBER, CITY, COUNTRY,
                 DESCRIPTION, ROOMS_DEFINITION);
 
-        assertThatHasOwnerId(actual, OWNER_ID);
-        assertThatHasDescription(actual, DESCRIPTION);
-        assertThatHasAddress(actual, STREET, POSTAL_CODE, HOUSE_NUMBER, APARTMENT_NUMBER, CITY, COUNTRY);
-        assertThatHasRooms(actual, ROOMS_DEFINITION);
-    }
-
-    private void assertThatHasOwnerId(Apartment actual, String ownerId) {
-        assertThat(actual).hasFieldOrPropertyWithValue("ownerId", ownerId);
-    }
-
-    private void assertThatHasDescription(Apartment actual, String description) {
-        assertThat(actual).hasFieldOrPropertyWithValue("description", description);
-    }
-
-    private void assertThatHasAddress(
-            Apartment actual, String street, String postalCode, String houseNumber,
-            String apartmentNumber, String city, String country) {
-        assertThat(actual).extracting("address")
-                .hasFieldOrPropertyWithValue("street", street)
-                .hasFieldOrPropertyWithValue("postalCode", postalCode)
-                .hasFieldOrPropertyWithValue("houseNumber", houseNumber)
-                .hasFieldOrPropertyWithValue("apartmentNumber", apartmentNumber)
-                .hasFieldOrPropertyWithValue("city", city)
-                .hasFieldOrPropertyWithValue("country", country);
-    }
-
-    private void assertThatHasRooms(Apartment actual, Map<String, Double> roomsDefinition) {
-        assertThat(actual).extracting("rooms").satisfies(roomsActual -> {
-            List<Room> rooms = (List<Room>) roomsActual;
-            assertThat(rooms).hasSize(roomsDefinition.size());
-
-            roomsDefinition.forEach((name, squareMeter) -> {
-                assertThat(rooms).anySatisfy(hasRoomThat(name, squareMeter));
-            });
-        });
-    }
-
-    private Consumer<Room> hasRoomThat(String name, Double squareMeter) {
-        return room -> assertThat(room)
-                .hasFieldOrPropertyWithValue("name", name)
-                .hasFieldOrPropertyWithValue("squareMeter.size", squareMeter);
+        ApartmentAssertion.assertThat(actual)
+                .hasOwnerIdEqualsTo(OWNER_ID)
+                .hasDescriptionEqualsTo(DESCRIPTION)
+                .hasAddressEqualsTo(STREET, POSTAL_CODE, HOUSE_NUMBER, APARTMENT_NUMBER, CITY, COUNTRY)
+                .hasRoomsEqualsTo(ROOMS_DEFINITION);
     }
 }
