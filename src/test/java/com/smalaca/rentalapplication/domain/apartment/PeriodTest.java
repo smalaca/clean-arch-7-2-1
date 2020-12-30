e5dbd17578eaa16d07a26ac0ae87de1ff6ad5a6f
smalaca <ab13.krakow@gmail.com>
2020-12-30 01:55:05 +0100

    Unit Tests - Period - add test when start and end dates are the same

--- src/test/java/com/smalaca/rentalapplication/domain/apartment/PeriodTest.java
+++ src/test/java/com/smalaca/rentalapplication/domain/apartment/PeriodTest.java
@@ -1,5 +1,6 @@
 package com.smalaca.rentalapplication.domain.apartment;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -35,5 +36,14 @@ class PeriodTest {
                                 LocalDate.of(2020, 5, 5), LocalDate.of(2020, 5, 6), LocalDate.of(2020, 5, 7),
                                 LocalDate.of(2020, 5, 8), LocalDate.of(2020, 5, 9), LocalDate.of(2020, 5, 10)))
         );
+    }
+
+    @Test
+    void shouldReturnOneDateWhenStartAndEndAreTheSame() {
+        LocalDate date = LocalDate.of(2020, 10, 11);
+
+        List<LocalDate> actual = new Period(date, date).asDays();
+
+        assertThat(actual).containsExactly(date);
     }
 }
