032bce061fdb4a78996b6c55d2f77181c983d60e
smalaca <ab13.krakow@gmail.com>
2020-12-30 20:58:45 +0100

    Apartment - book - add tests of Booking creation and event publishing

--- src/test/java/com/smalaca/rentalapplication/domain/apartment/ApartmentTest.java
+++ src/test/java/com/smalaca/rentalapplication/domain/apartment/ApartmentTest.java
@@ -1,8 +1,14 @@
 package com.smalaca.rentalapplication.domain.apartment;
 
 import com.google.common.collect.ImmutableMap;
+import com.smalaca.rentalapplication.domain.eventchannel.EventChannel;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 
+import java.time.LocalDate;
 import java.util.Map;
 
 class ApartmentTest {
@@ -15,19 +21,51 @@ class ApartmentTest {
     private static final String COUNTRY = "Poland";
     private static final String DESCRIPTION = "Nice place to stay";
     private static final Map<String, Double> ROOMS_DEFINITION = ImmutableMap.of("Toilet", 10.0, "Bedroom", 30.0);
+    private static final String TENANT_IT = "137";
+    private static final LocalDate START = LocalDate.of(2020, 3, 4);
+    private static final LocalDate MIDDLE = LocalDate.of(2020, 3, 5);
+    private static final LocalDate END = LocalDate.of(2020, 3, 6);
+    private static final Period PERIOD = new Period(START, END);
 
     private final ApartmentFactory apartmentFactory = new ApartmentFactory();
+    private final EventChannel eventChannel = Mockito.mock(EventChannel.class);
 
     @Test
     void shouldCreateApartmentWithAllRequiredFields() {
-        Apartment actual = apartmentFactory.create(
-                OWNER_ID, STREET, POSTAL_CODE, HOUSE_NUMBER, APARTMENT_NUMBER, CITY, COUNTRY,
-                DESCRIPTION, ROOMS_DEFINITION);
+        Apartment actual = createApartment();
 
         ApartmentAssertion.assertThat(actual)
                 .hasOwnerIdEqualsTo(OWNER_ID)
                 .hasDescriptionEqualsTo(DESCRIPTION)
                 .hasAddressEqualsTo(STREET, POSTAL_CODE, HOUSE_NUMBER, APARTMENT_NUMBER, CITY, COUNTRY)
                 .hasRoomsEqualsTo(ROOMS_DEFINITION);
+    }
+
+    @Test
+    void shouldCreateBookingOnceBooked() {
+        Apartment apartment = createApartment();
+
+        Booking actual = apartment.book(TENANT_IT, PERIOD, eventChannel);
+
+        BookingAssertion.assertThat(actual)
+                .isApartment()
+                .hasTenantIdEqualTo(TENANT_IT)
+                .containsAllDays(START, MIDDLE, END);
+    }
+
+    @Test
+    void shouldPublishApartmentBooked() {
+        ArgumentCaptor captor = ArgumentCaptor.forClass(ApartmentBooked.class);
+        Apartment apartment = createApartment();
+
+        apartment.book(TENANT_IT, PERIOD, eventChannel);
+
+        BDDMockito.then(eventChannel).should().publish(ArgumentMatchers.any(ApartmentBooked.class));
+    }
+
+    private Apartment createApartment() {
+        return apartmentFactory.create(
+                OWNER_ID, STREET, POSTAL_CODE, HOUSE_NUMBER, APARTMENT_NUMBER, CITY, COUNTRY,
+                DESCRIPTION, ROOMS_DEFINITION);
     }
 }
