adb0123faeb8d1de58e8547ca2b57ae59b28278b
smalaca <ab13.krakow@gmail.com>
2020-12-30 01:53:13 +0100

    Unit Tests - Period - introduce parametrized tests of asDays method

--- src/test/java/com/smalaca/rentalapplication/domain/apartment/PeriodTest.java
+++ src/test/java/com/smalaca/rentalapplication/domain/apartment/PeriodTest.java
@@ -1,22 +1,39 @@
 package com.smalaca.rentalapplication.domain.apartment;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PeriodTest {
 
-    @Test
-    void shouldReturnAllDaysBetweenStartAndEnd() {
-        LocalDate start = LocalDate.of(2020, 1, 1);
-        LocalDate end = LocalDate.of(2020, 1, 3);
-        Period period = new Period(start, end);
+    @ParameterizedTest
+    @MethodSource("daysBetweenStartAndEnd")
+    void shouldReturnAllDaysBetweenStartAndEnd(LocalDate start, LocalDate end, Iterable<LocalDate> expected) {
+        List<LocalDate> actual = new Period(start, end).asDays();
 
-        List<LocalDate> actual = period.asDays();
+        assertThat(actual).containsExactlyElementsOf(expected);
+    }
 
-        assertThat(actual).containsExactly(start, LocalDate.of(2020, 1, 2), end);
+    private static Stream<Arguments> daysBetweenStartAndEnd() {
+        return Stream.of(
+                Arguments.of(
+                        LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 3),
+                        asList(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 2), LocalDate.of(2020, 1, 3))),
+                Arguments.of(
+                        LocalDate.of(2020, 10, 1), LocalDate.of(2020, 10, 2),
+                        asList(LocalDate.of(2020, 10, 1), LocalDate.of(2020, 10, 2))),
+                Arguments.of(
+                        LocalDate.of(2020, 5, 5), LocalDate.of(2020, 5, 10),
+                        asList(
+                                LocalDate.of(2020, 5, 5), LocalDate.of(2020, 5, 6), LocalDate.of(2020, 5, 7),
+                                LocalDate.of(2020, 5, 8), LocalDate.of(2020, 5, 9), LocalDate.of(2020, 5, 10)))
+        );
     }
 }
