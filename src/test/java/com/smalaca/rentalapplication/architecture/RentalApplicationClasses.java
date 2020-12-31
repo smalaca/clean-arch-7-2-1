be18ce5e88d2bb4f8028d913f7873e0a99c44bcd
smalaca <ab13.krakow@gmail.com>
2020-12-31 11:37:43 +0100

    Architecture Tests - extract RentalApplicationClasses to separate JavaClasses definition from tests

---
+++ src/test/java/com/smalaca/rentalapplication/architecture/RentalApplicationClasses.java
@@ -0,0 +1,14 @@
+package com.smalaca.rentalapplication.architecture;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+
+class RentalApplicationClasses {
+    static JavaClasses get() {
+        return new ClassFileImporter()
+                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_JARS)
+                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+                .importPackages("com.smalaca.rentalapplication");
+    }
+}
