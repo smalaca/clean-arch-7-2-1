cee4bda52eec2d251c208ba72bf9d8100e09f86c
smalaca <ab13.krakow@gmail.com>
2020-12-31 11:56:11 +0100

    Architecture Tests - verify if package separation is fine

--- src/test/java/com/smalaca/rentalapplication/architecture/PackageStructureTest.java
+++ src/test/java/com/smalaca/rentalapplication/architecture/PackageStructureTest.java
@@ -1,14 +1,45 @@
 package com.smalaca.rentalapplication.architecture;
 
-import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.core.domain.JavaClasses;
 import org.junit.jupiter.api.Test;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
 class PackageStructureTest {
+    private static final String DOMAIN = "..domain..";
+    private static final String APPLICATION = "..application..";
+    private static final String JAVA = "java..";
+    private static final String QUERY = "..query..";
+    private static final String INFRASTRUCTURE = "..infrastructure..";
+
+    private final JavaClasses classes = RentalApplicationClasses.get();
+
     @Test
     void domainShouldTalkOnlyWithDomain() {
-        ArchRuleDefinition.classes()
-                .that().resideInAPackage("..domain..")
-                .should().onlyAccessClassesThat().resideInAnyPackage("..domain..", "java..")
-                .check(RentalApplicationClasses.get());
+        classes().that().resideInAPackage(DOMAIN)
+                .should().onlyAccessClassesThat().resideInAnyPackage(DOMAIN, JAVA)
+                .check(classes);
+    }
+
+    @Test
+    void applicationShouldTalkOnlyWithApplicationAndDomain() {
+        classes().that().resideInAPackage(APPLICATION)
+                .should().onlyAccessClassesThat().resideInAnyPackage(APPLICATION, DOMAIN, JAVA)
+                .check(classes);
+    }
+
+    @Test
+    void queryShouldTalkOnlyWithQuery() {
+        classes().that().resideInAPackage(QUERY)
+                .should().onlyAccessClassesThat().resideInAnyPackage(QUERY, JAVA)
+                .check(classes);
+    }
+
+    @Test
+    void infrastructureShouldNotTalkWithDomain() {
+        classes().that().resideInAPackage(INFRASTRUCTURE)
+                .should().onlyAccessClassesThat().resideOutsideOfPackage(DOMAIN)
+                .orShould().haveSimpleNameContaining("Repository")
+                .check(classes);
     }
 }
