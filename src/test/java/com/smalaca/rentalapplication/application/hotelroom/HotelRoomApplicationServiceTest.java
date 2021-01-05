d172d3de8e5df44cca737a452b20bf4cf378a05f
smalaca <ab13.krakow@gmail.com>
2021-01-05 21:35:13 +0100

    add missing tests of HotelRoomApplicationService

--- src/test/java/com/smalaca/rentalapplication/application/hotelroom/HotelRoomApplicationServiceTest.java
+++ src/test/java/com/smalaca/rentalapplication/application/hotelroom/HotelRoomApplicationServiceTest.java
@@ -6,8 +6,10 @@ import com.smalaca.rentalapplication.domain.apartment.BookingAssertion;
 import com.smalaca.rentalapplication.domain.apartment.BookingRepository;
 import com.smalaca.rentalapplication.domain.eventchannel.EventChannel;
 import com.smalaca.rentalapplication.domain.hotelroom.HotelRoom;
+import com.smalaca.rentalapplication.domain.hotelroom.HotelRoomAssertion;
 import com.smalaca.rentalapplication.domain.hotelroom.HotelRoomFactory;
 import com.smalaca.rentalapplication.domain.hotelroom.HotelRoomRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.BDDMockito;
@@ -15,17 +17,51 @@ import org.mockito.Mockito;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import static java.util.Arrays.asList;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 class HotelRoomApplicationServiceTest {
+    private static final String HOTEL_ID = UUID.randomUUID().toString();
+    private static final int ROOM_NUMBER = 13;
+    private static final Map<String, Double> SPACES_DEFINITION = ImmutableMap.of("RoomOne", 20.0, "RoomTwo", 20.0);
+    private static final String DESCRIPTION = "What a lovely place";
     private static final String TENANT_ID = "4321";
     private static final List<LocalDate> DAYS = asList(LocalDate.now(), LocalDate.now().plusDays(1));
+    private static final String HOTEL_ROOM_ID = "7821321";
 
     private final HotelRoomRepository hotelRoomRepository = Mockito.mock(HotelRoomRepository.class);
     private final BookingRepository bookingRepository = Mockito.mock(BookingRepository.class);
     private final EventChannel eventChannel = Mockito.mock(EventChannel.class);
     private final HotelRoomApplicationService service = new HotelRoomApplicationService(hotelRoomRepository, bookingRepository, eventChannel);
+    private final HotelRoomFactory factory = new HotelRoomFactory();
+
+    @Test
+    void shouldCreateHotelRoom() {
+        ArgumentCaptor<HotelRoom> captor = ArgumentCaptor.forClass(HotelRoom.class);
+
+        service.add(HOTEL_ID, ROOM_NUMBER, SPACES_DEFINITION, DESCRIPTION);
+
+        then(hotelRoomRepository).should().save(captor.capture());
+        HotelRoomAssertion.assertThat(captor.getValue())
+                .hasHotelIdEqualTo(HOTEL_ID)
+                .hasRoomNumberEqualTo(ROOM_NUMBER)
+                .hasSpacesDefinitionEqualTo(SPACES_DEFINITION)
+                .hasDescriptionEqualTo(DESCRIPTION);
+    }
+
+    @Test
+    void shouldReturnIdOfNewHotelRoom() {
+        given(hotelRoomRepository.save(any())).willReturn(HOTEL_ROOM_ID);
+
+        String actual = service.add(HOTEL_ID, ROOM_NUMBER, SPACES_DEFINITION, DESCRIPTION);
+
+        Assertions.assertThat(actual).isEqualTo(HOTEL_ROOM_ID);
+    }
 
     @Test
     void shouldCreateBookingWhenHotelRoomBooked() {
@@ -53,6 +89,6 @@ class HotelRoomApplicationServiceTest {
     }
 
     private HotelRoom createHotelRoom() {
-        return new HotelRoomFactory().create("5678", 42, ImmutableMap.of("Room1", 30.0), "This is very nice place");
+        return factory.create(HOTEL_ID, ROOM_NUMBER, SPACES_DEFINITION, DESCRIPTION);
     }
 }
