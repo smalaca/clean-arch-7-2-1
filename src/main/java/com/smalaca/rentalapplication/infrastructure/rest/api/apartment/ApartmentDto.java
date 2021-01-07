27a13174ab227bcf3e06982fa219a13cf4283ff8
smalaca <ab13.krakow@gmail.com>
2021-01-07 20:48:02 +0100

    Introduce Lombok to remove boilerplate code from ApartmentDto and ApartmentBookingDto

--- src/main/java/com/smalaca/rentalapplication/infrastructure/rest/api/apartment/ApartmentDto.java
+++ src/main/java/com/smalaca/rentalapplication/infrastructure/rest/api/apartment/ApartmentDto.java
@@ -1,7 +1,12 @@
 package com.smalaca.rentalapplication.infrastructure.rest.api.apartment;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.Map;
 
+@AllArgsConstructor
+@Getter
 public class ApartmentDto {
     private final String ownerId;
     private final String street;
@@ -12,54 +17,4 @@ public class ApartmentDto {
     private final String country;
     private final String description;
     private final Map<String, Double> roomsDefinition;
-
-    public ApartmentDto(
-            String ownerId, String street, String postalCode, String houseNumber, String apartmentNumber,
-            String city, String country, String description, Map<String, Double> roomsDefinition) {
-        this.ownerId = ownerId;
-        this.street = street;
-        this.postalCode = postalCode;
-        this.houseNumber = houseNumber;
-        this.apartmentNumber = apartmentNumber;
-        this.city = city;
-        this.country = country;
-        this.description = description;
-        this.roomsDefinition = roomsDefinition;
-    }
-
-    public String getOwnerId() {
-        return ownerId;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    public String getApartmentNumber() {
-        return apartmentNumber;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Map<String, Double> getRoomsDefinition() {
-        return roomsDefinition;
-    }
 }
