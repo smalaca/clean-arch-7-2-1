2f8a501a45506585dfdbd2d306da70a0bc27a377
smalaca <ab13.krakow@gmail.com>
2021-01-07 21:12:03 +0100

    Use Lombok to remove boilerplate code from HotelDto, HotelRoomDto and HotelBookingDto

--- src/main/java/com/smalaca/rentalapplication/infrastructure/rest/api/hotelroom/HotelBookingDto.java
+++ src/main/java/com/smalaca/rentalapplication/infrastructure/rest/api/hotelroom/HotelBookingDto.java
@@ -1,22 +1,14 @@
 package com.smalaca.rentalapplication.infrastructure.rest.api.hotelroom;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@AllArgsConstructor
+@Getter
 public class HotelBookingDto {
     private final String tenantId;
     private final List<LocalDate> days;
-
-    public HotelBookingDto(String tenantId, List<LocalDate> days) {
-        this.tenantId = tenantId;
-        this.days = days;
-    }
-
-    public String getTenantId() {
-        return tenantId;
-    }
-
-    public List<LocalDate> getDays() {
-        return days;
-    }
 }
--- src/main/java/com/smalaca/rentalapplication/infrastructure/rest/api/hotelroom/HotelRoomDto.java
+++ src/main/java/com/smalaca/rentalapplication/infrastructure/rest/api/hotelroom/HotelRoomDto.java
@@ -1,33 +1,15 @@
 package com.smalaca.rentalapplication.infrastructure.rest.api.hotelroom;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.Map;
 
+@AllArgsConstructor
+@Getter
 public class HotelRoomDto {
     private final String hotelId;
     private final int number;
     private final Map<String, Double> spacesDefinition;
     private final String description;
-
-    public HotelRoomDto(String hotelId, int number, Map<String, Double> spacesDefinition, String description) {
-        this.hotelId = hotelId;
-        this.number = number;
-        this.spacesDefinition = spacesDefinition;
-        this.description = description;
-    }
-
-    public String getHotelId() {
-        return hotelId;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public Map<String, Double> getSpacesDefinition() {
-        return spacesDefinition;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
