90d9233748169f7fb83b1fe82b69e69460decc3e
smalaca <ab13.krakow@gmail.com>
2021-01-08 20:45:04 +0100

    ApartmentOffer - TDD - REFACTOR - introduce builder to create an object

--- src/main/java/com/smalaca/rentalapplication/application/apartmentoffer/ApartmentOfferService.java
+++ src/main/java/com/smalaca/rentalapplication/application/apartmentoffer/ApartmentOfferService.java
@@ -1,12 +1,12 @@
 package com.smalaca.rentalapplication.application.apartmentoffer;
 
-import com.smalaca.rentalapplication.domain.apartmentoffer.ApartmentAvailability;
-import com.smalaca.rentalapplication.domain.apartmentoffer.ApartmentOfferRepository;
-import com.smalaca.rentalapplication.domain.apartmentoffer.Money;
 import com.smalaca.rentalapplication.domain.apartmentoffer.ApartmentOffer;
+import com.smalaca.rentalapplication.domain.apartmentoffer.ApartmentOfferRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import static com.smalaca.rentalapplication.domain.apartmentoffer.ApartmentOffer.Builder.apartmentOffer;
 
 class ApartmentOfferService {
     private final ApartmentOfferRepository repository;
@@ -16,7 +16,11 @@ class ApartmentOfferService {
     }
 
     void add(String apartmentId, BigDecimal price, LocalDate start, LocalDate end) {
-        ApartmentOffer apartmentOffer = new ApartmentOffer(apartmentId, new Money(price), new ApartmentAvailability(start, end));
+        ApartmentOffer apartmentOffer = apartmentOffer()
+            .withApartmentId(apartmentId)
+            .withPrice(price)
+            .withAvailability(start, end)
+            .build();
         repository.save(apartmentOffer);
     }
 }
--- src/main/java/com/smalaca/rentalapplication/domain/apartmentoffer/ApartmentOffer.java
+++ src/main/java/com/smalaca/rentalapplication/domain/apartmentoffer/ApartmentOffer.java
@@ -1,5 +1,8 @@
 package com.smalaca.rentalapplication.domain.apartmentoffer;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class ApartmentOffer {
     private final String apartmentId;
     private final Money money;
@@ -9,5 +12,46 @@ public class ApartmentOffer {
         this.apartmentId = apartmentId;
         this.money = money;
         this.availability = availability;
+    }
+
+    public static class Builder {
+        private String apartmentId;
+        private BigDecimal price;
+        private LocalDate start;
+        private LocalDate end;
+
+        private Builder() {}
+        
+        public static Builder apartmentOffer() {
+            return new Builder();
+        }
+
+        public Builder withApartmentId(String apartmentId) {
+            this.apartmentId = apartmentId;
+            return this;
+        }
+
+        public Builder withPrice(BigDecimal price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder withAvailability(LocalDate start, LocalDate end) {
+            this.start = start;
+            this.end = end;
+            return this;
+        }
+
+        public ApartmentOffer build() {
+            return new ApartmentOffer(apartmentId, money(), availability());
+        }
+
+        private ApartmentAvailability availability() {
+            return new ApartmentAvailability(start, end);
+        }
+
+        private Money money() {
+            return new Money(price);
+        }
     }
 }
