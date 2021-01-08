4fcb27e1b0edbb0ee62c89d8729324404b0c4e28
smalaca <ab13.krakow@gmail.com>
2021-01-08 23:17:08 +0100

    let domain to use apache common classes

--- src/test/java/com/smalaca/rentalapplication/architecture/PackageStructureTest.java
+++ src/test/java/com/smalaca/rentalapplication/architecture/PackageStructureTest.java
@@ -11,6 +11,7 @@ class PackageStructureTest {
     private static final String DOMAIN = "..domain..";
     private static final String APPLICATION = "..application..";
     private static final String JAVA = "java..";
+    private static final String APACHE_COMMONS = "org.apache.commons.lang3..";
     private static final String QUERY = "..query..";
     private static final String INFRASTRUCTURE = "..infrastructure..";
 
@@ -19,7 +20,7 @@ class PackageStructureTest {
     @Test
     void domainShouldTalkOnlyWithDomain() {
         classes().that().resideInAPackage(DOMAIN)
-                .should().onlyAccessClassesThat().resideInAnyPackage(DOMAIN, JAVA)
+                .should().onlyAccessClassesThat().resideInAnyPackage(DOMAIN, JAVA, APACHE_COMMONS)
                 .check(classes);
     }
 
