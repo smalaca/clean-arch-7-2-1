9f2c1018315ad7da7848c5d184ca8f289dc3507c
smalaca <ab13.krakow@gmail.com>
2021-01-08 22:12:53 +0100

    HotelRoomOffer - TDD - REFACT - dicrease visibility of Money constructor

--- src/main/java/com/smalaca/rentalapplication/domain/hotelroomoffer/Money.java
+++ src/main/java/com/smalaca/rentalapplication/domain/hotelroomoffer/Money.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 class Money {
     private final BigDecimal value;
 
-    Money(BigDecimal value) {
+    private Money(BigDecimal value) {
         this.value = value;
     }
 
