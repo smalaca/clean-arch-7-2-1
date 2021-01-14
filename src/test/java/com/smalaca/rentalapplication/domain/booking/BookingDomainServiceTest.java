ac39746f892c5331163c018d71f7e311d2301069
smalaca <ab13.krakow@gmail.com>
2021-01-14 23:17:14 +0100

    BookingDomainService - add test to verify if BookingAccepted is published when booking accepted

--- src/test/java/com/smalaca/rentalapplication/domain/booking/BookingDomainServiceTest.java
+++ src/test/java/com/smalaca/rentalapplication/domain/booking/BookingDomainServiceTest.java
@@ -3,7 +3,10 @@ package com.smalaca.rentalapplication.domain.booking;
 import com.smalaca.rentalapplication.domain.clock.Clock;
 import com.smalaca.rentalapplication.domain.event.EventIdFactory;
 import com.smalaca.rentalapplication.domain.eventchannel.EventChannel;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,6 +19,7 @@ class BookingDomainServiceTest {
     private static final String RENTAL_PLACE_ID = "1234";
     private static final String TENANT_ID = "5678";
     private static final List<LocalDate> DAYS = asList(LocalDate.now(), LocalDate.now().plusDays(1));
+    private static final List<Booking> NO_BOOKINGS_FOUND = emptyList();
 
     private final EventIdFactory eventIdFactory = mock(EventIdFactory.class);
     private final Clock clock = mock(Clock.class);
@@ -24,10 +28,28 @@ class BookingDomainServiceTest {
 
     @Test
     void shouldAcceptBookingWhenNoOtherBookingsFound() {
-        Booking booking = Booking.hotelRoom(RENTAL_PLACE_ID, TENANT_ID, DAYS);
+        Booking booking = givenBooking();
 
         service.accept(booking, emptyList());
 
         BookingAssertion.assertThat(booking).isAccepted();
+    }
+
+    @Test
+    void shouldPublishBookingAcceptedEventWhenBookingIsAccepted() {
+        ArgumentCaptor<BookingAccepted> captor = ArgumentCaptor.forClass(BookingAccepted.class);
+
+        service.accept(givenBooking(), NO_BOOKINGS_FOUND);
+
+        BDDMockito.then(eventChannel).should().publish(captor.capture());
+        BookingAccepted actual = captor.getValue();
+        Assertions.assertThat(actual.getRentalType()).isEqualTo("HOTEL_ROOM");
+        Assertions.assertThat(actual.getRentalPlaceId()).isEqualTo(RENTAL_PLACE_ID);
+        Assertions.assertThat(actual.getTenantId()).isEqualTo(TENANT_ID);
+        Assertions.assertThat(actual.getDays()).containsExactlyElementsOf(DAYS);
+    }
+
+    private Booking givenBooking() {
+        return Booking.hotelRoom(RENTAL_PLACE_ID, TENANT_ID, DAYS);
     }
 }
